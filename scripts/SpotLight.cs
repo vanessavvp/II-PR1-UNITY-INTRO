2acac7ffd2d25297b924fb01347f1db51aa0d36e
vanessavillalba <alu0101265704@ull.edu.es>
2021-10-21 23:15:59 +0100

    Uploading scripts

---
+++ scripts/SpotLight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotLight : MonoBehaviour
+{
+    const int ID = 6;
+    int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Object name: " + this.name + "\tTag: " + this.tag + "\tID: " + ID + "\tcounter: " + counter++);  
+    }
+}
